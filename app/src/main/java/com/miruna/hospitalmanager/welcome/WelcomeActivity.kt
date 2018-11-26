6c0e814b428e09059b98fb084fcba351b1b23c69
MirunaRux <miruna_ruxandra97@yahoo.com>
2018-11-26 18:01:40 +0200

    Template Activity for Welcome Screen

---
+++ app/src/main/java/com/miruna/hospitalmanager/welcome/WelcomeActivity.kt
@@ -0,0 +1,13 @@
+package com.miruna.hospitalmanager.welcome
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.miruna.hospitalmanager.R
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
